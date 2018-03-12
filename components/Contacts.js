5d4ab9a2cca8f04f41c22d698ffa6e5fbf02bcb9
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-03-13 00:28:18 +0100

    Adding again removed folder

---
+++ components/Contacts.js
@@ -0,0 +1,41 @@
+/* var Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+    var contacts = this.props.items.map(function(contact) {
+        return React.createElement(Contact, {item: contact, key: contact.id});
+    });
+
+    return (
+      React.createElement('ul', {className: 'contactsList'}, contacts)
+    );
+  }
+}); */
+
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      <div className='contactItem'>
+        <img className='contactImage'
+           src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+        <div className="contactContent">
+          <p className="contactLabel">
+            {this.props.item.firstName}
+          </p>
+          <p className="contactLabel">
+            {this.props.item.lastName}
+          </p>
+          <a className="contactEmail" href={'mailto:' + this.props.item.email}>
+            {this.props.item.email}
+          </a>
+        </div>
+      </div>
+    );
+  }
+});
