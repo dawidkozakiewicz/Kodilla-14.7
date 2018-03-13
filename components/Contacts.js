ec94bda71b492b5fec8f53bf700935dc6fccf054
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-03-13 22:36:34 +0100

    Changes in JS files

--- components/Contacts.js
+++ components/Contacts.js
@@ -14,28 +14,20 @@
   }
 }); */
 
-var Contact = React.createClass({
+var Contacts = React.createClass({
   propTypes: {
-    item: React.PropTypes.object.isRequired,
+    items: React.PropTypes.array.isRequired,
   },
 
   render: function() {
+    var contacts = this.props.items.map(function(contact) {
+      return React.createElement(Contact, {item: contact, key: contact.id});
+    });
+
     return (
-      <div className='contactItem'>
-        <img className='contactImage'
-           src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
-        <div className="contactContent">
-          <p className="contactLabel">
-            {this.props.item.firstName}
-          </p>
-          <p className="contactLabel">
-            {this.props.item.lastName}
-          </p>
-          <a className="contactEmail" href={'mailto:' + this.props.item.email}>
-            {this.props.item.email}
-          </a>
-        </div>
-      </div>
+      <ul className='contactList'>
+        {contacts}
+      </ul>
     );
   }
 });
